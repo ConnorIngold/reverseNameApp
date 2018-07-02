7862d781e2d5210ff06f46bd1fe232b7c4e14cd1
connor ingold <connoringold@gmail.com>
2018-07-02 21:29:50 +0100

    done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // var name = document.getElementById('name')
 // console.log('hi')
 
-var forward = prompt('what your name?')
-
-var backward = forward.split("").reverse().join("");
-
-document.write("<h1> " + backward + " </h1>");
+// var forward = prompt('what your name?')
+//
+// var backward = forward.split("").reverse().join("");
+//
+// document.write("<h1> " + backward + " </h1>");
